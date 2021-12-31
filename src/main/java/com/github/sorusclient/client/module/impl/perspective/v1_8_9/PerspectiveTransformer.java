b66329c2a74be40e1cb96c889f14476bdacf3064
danterusdev <danterusdev@gmail.com>
2021-12-31 10:49:25 -0800

    Perspective: Use Look Direction For Most Rendering

--- src/main/java/com/github/sorusclient/client/module/impl/perspective/v1_8_9/PerspectiveTransformer.java
+++ src/main/java/com/github/sorusclient/client/module/impl/perspective/v1_8_9/PerspectiveTransformer.java
@@ -19,6 +19,10 @@ public class PerspectiveTransformer implements Listener, ITransformer {
         {
             put(Identifier.parse("v1_8_9/net/minecraft/entity/Entity").getClassName(), PerspectiveTransformer.this::transformEntity);
             put(Identifier.parse("v1_8_9/net/minecraft/client/render/GameRenderer").getClassName(), PerspectiveTransformer.this::transformGameRenderer);
+            put(Identifier.parse("v1_8_9/net/minecraft/client/render/entity/EntityRenderDispatcher").getClassName(), PerspectiveTransformer.this::transformEntityRenderDispatcher);
+            put(Identifier.parse("v1_8_9/net/minecraft/client/render/WorldRenderer").getClassName(), PerspectiveTransformer.this::transformWorldRenderer);
+            put(Identifier.parse("v1_8_9/net/minecraft/client/particle/ParticleManager").getClassName(), PerspectiveTransformer.this::transformParticleManager);
+            put(Identifier.parse("v1_8_9/net/minecraft/client/class_321").getClassName(), PerspectiveTransformer.this::transformClass321);
         }
     };
 
@@ -66,34 +70,82 @@ public class PerspectiveTransformer implements Listener, ITransformer {
     private void transformGameRenderer(ClassNode classNode) {
         Identifier transformCamera = Identifier.parse("v1_8_9/net/minecraft/client/render/GameRenderer#transformCamera(F)V");
 
+        for (MethodNode methodNode : classNode.methods) {
+            if (methodNode.name.equals(transformCamera.getMethodName()) && methodNode.desc.equals(transformCamera.getMethodDesc())) {
+                this.transformRotationCalls(methodNode);
+            }
+        }
+    }
+
+    private void transformEntityRenderDispatcher(ClassNode classNode) {
+        Identifier transformCamera = Identifier.parse("v1_8_9/net/minecraft/client/render/entity/EntityRenderDispatcher#method_10200(Lv1_8_9/net/minecraft/world/World;Lv1_8_9/net/minecraft/client/font/TextRenderer;Lv1_8_9/net/minecraft/entity/Entity;Lv1_8_9/net/minecraft/entity/Entity;Lv1_8_9/net/minecraft/client/options/GameOptions;F)V");
+
+        for (MethodNode methodNode : classNode.methods) {
+            if (methodNode.name.equals(transformCamera.getMethodName()) && methodNode.desc.equals(transformCamera.getMethodDesc())) {
+                this.transformRotationCalls(methodNode);
+            }
+        }
+    }
+
+    private void transformWorldRenderer(ClassNode classNode) {
+        Identifier method_9906 = Identifier.parse("v1_8_9/net/minecraft/client/render/WorldRenderer#method_9906(Lv1_8_9/net/minecraft/entity/Entity;DLv1_8_9/net/minecraft/client/render/debug/CameraView;IZ)V");
+
+        for (MethodNode methodNode : classNode.methods) {
+            if (methodNode.name.equals(method_9906.getMethodName()) && methodNode.desc.equals(method_9906.getMethodDesc())) {
+                this.transformRotationCalls(methodNode);
+            }
+        }
+    }
+
+    private void transformParticleManager(ClassNode classNode) {
+        Identifier method_1299 = Identifier.parse("v1_8_9/net/minecraft/client/particle/ParticleManager#method_1299(Lv1_8_9/net/minecraft/entity/Entity;F)V");
+
+        for (MethodNode methodNode : classNode.methods) {
+            if (methodNode.name.equals(method_1299.getMethodName()) && methodNode.desc.equals(method_1299.getMethodDesc())) {
+                this.transformRotationCalls(methodNode);
+            }
+        }
+    }
+
+    private void transformClass321(ClassNode classNode) {
+        Identifier method_1299 = Identifier.parse("v1_8_9/net/minecraft/client/class_321#method_804(Lv1_8_9/net/minecraft/entity/player/PlayerEntity;Z)V");
+
+        for (MethodNode methodNode : classNode.methods) {
+            if (methodNode.name.equals(method_1299.getMethodName()) && methodNode.desc.equals(method_1299.getMethodDesc())) {
+                this.transformRotationCalls(methodNode);
+            }
+        }
+    }
+
+    private void transformRotationCalls(MethodNode methodNode) {
+        Identifier entity = Identifier.parse("v1_8_9/net/minecraft/entity/Entity");
+        Identifier playerEntity = Identifier.parse("v1_8_9/net/minecraft/entity/player/PlayerEntity");
+
         Identifier pitch = Identifier.parse("v1_8_9/net/minecraft/entity/Entity#pitch");
         Identifier prevPitch = Identifier.parse("v1_8_9/net/minecraft/entity/Entity#prevPitch");
         Identifier yaw = Identifier.parse("v1_8_9/net/minecraft/entity/Entity#yaw");
         Identifier prevYaw = Identifier.parse("v1_8_9/net/minecraft/entity/Entity#prevYaw");
 
-        for (MethodNode methodNode : classNode.methods) {
-            if (methodNode.name.equals(transformCamera.getMethodName()) && methodNode.desc.equals(transformCamera.getMethodDesc())) {
-                for (AbstractInsnNode insnNode : methodNode.instructions) {
-                    if (insnNode instanceof FieldInsnNode && ((FieldInsnNode) insnNode).owner.equals(pitch.getClassName()) && ((FieldInsnNode) insnNode).name.equals(pitch.getFieldName())) {
-                        methodNode.instructions.insert(insnNode, new MethodInsnNode(Opcodes.INVOKESTATIC,
-                                PerspectiveHook.class.getName().replace(".", "/"), "modifyPitch", "(F)F"));
-                    }
+        for (AbstractInsnNode insnNode : methodNode.instructions) {
+            if (insnNode instanceof FieldInsnNode && (((FieldInsnNode) insnNode).owner.equals(entity.getClassName()) || ((FieldInsnNode) insnNode).owner.equals(playerEntity.getClassName())) && ((FieldInsnNode) insnNode).name.equals(pitch.getFieldName())) {
+                methodNode.instructions.insert(insnNode, new MethodInsnNode(Opcodes.INVOKESTATIC,
+                        PerspectiveHook.class.getName().replace(".", "/"), "modifyPitch", "(F)F"));
+            }
 
-                    if (insnNode instanceof FieldInsnNode && ((FieldInsnNode) insnNode).owner.equals(yaw.getClassName()) && ((FieldInsnNode) insnNode).name.equals(yaw.getFieldName())) {
-                        methodNode.instructions.insert(insnNode, new MethodInsnNode(Opcodes.INVOKESTATIC,
-                                PerspectiveHook.class.getName().replace(".", "/"), "modifyYaw", "(F)F"));
-                    }
+            if (insnNode instanceof FieldInsnNode && (((FieldInsnNode) insnNode).owner.equals(entity.getClassName()) || ((FieldInsnNode) insnNode).owner.equals(playerEntity.getClassName())) && ((FieldInsnNode) insnNode).name.equals(yaw.getFieldName())) {
+                methodNode.instructions.insert(insnNode, new MethodInsnNode(Opcodes.INVOKESTATIC,
+                        PerspectiveHook.class.getName().replace(".", "/"), "modifyYaw", "(F)F"));
+            }
 
-                    if (insnNode instanceof FieldInsnNode && ((FieldInsnNode) insnNode).owner.equals(prevPitch.getClassName()) && ((FieldInsnNode) insnNode).name.equals(prevPitch.getFieldName())) {
-                        methodNode.instructions.insert(insnNode, new MethodInsnNode(Opcodes.INVOKESTATIC,
-                                PerspectiveHook.class.getName().replace(".", "/"), "modifyPrevPitch", "(F)F"));
-                    }
+            if (insnNode instanceof FieldInsnNode && (((FieldInsnNode) insnNode).owner.equals(entity.getClassName()) || ((FieldInsnNode) insnNode).owner.equals(playerEntity.getClassName())) && ((FieldInsnNode) insnNode).name.equals(prevPitch.getFieldName())) {
+                methodNode.instructions.insert(insnNode, new MethodInsnNode(Opcodes.INVOKESTATIC,
+                        PerspectiveHook.class.getName().replace(".", "/"), "modifyPrevPitch", "(F)F"));
+            }
 
-                    if (insnNode instanceof FieldInsnNode && ((FieldInsnNode) insnNode).owner.equals(prevYaw.getClassName()) && ((FieldInsnNode) insnNode).name.equals(prevYaw.getFieldName())) {
-                        methodNode.instructions.insert(insnNode, new MethodInsnNode(Opcodes.INVOKESTATIC,
-                                PerspectiveHook.class.getName().replace(".", "/"), "modifyPrevYaw", "(F)F"));
-                    }
-                }
+            if (insnNode instanceof FieldInsnNode && (((FieldInsnNode) insnNode).owner.equals(entity.getClassName()) || ((FieldInsnNode) insnNode).owner.equals(playerEntity.getClassName())) && ((FieldInsnNode) insnNode).name.equals(prevYaw.getFieldName())) {
+                methodNode.instructions.insert(insnNode, new MethodInsnNode(Opcodes.INVOKESTATIC,
+                        PerspectiveHook.class.getName().replace(".", "/"), "modifyPrevYaw", "(F)F"));
+
             }
         }
     }
