07983bea5907d79b3a638570b33fdcaa68d387d8
danterusdev <danterusdev@gmail.com>
2022-04-18 20:00:48 -0700

    remove sorus argument from args passed to mc

--- src/main/kotlin/com/github/sorusclient/client/bootstrap/Launcher.kt
+++ src/main/kotlin/com/github/sorusclient/client/bootstrap/Launcher.kt
@@ -15,14 +15,19 @@ class Launcher {
 
         @JvmStatic
         fun main(args: Array<String>) {
-            val version = args[args.indexOf("--minecraftVersion") + 1]
+            val minecraftVersionIndex = args.indexOf("--minecraftVersion") + 1
+            val version = args[minecraftVersionIndex]
             BootstrapManager.loadJson(Launcher::class.java.classLoader.getResource("sorus.json")!!, version)
 
             Sorus.initialize()
 
             val main = Class.forName("net.minecraft.client.main.Main")
             val mainMethod = main.getMethod("main", Array<String>::class.java)
-            mainMethod.invoke(null, args)
+
+            val args2 = Array<String?>(args.size - 2) { null }
+            System.arraycopy(args, 0, args2, 0, minecraftVersionIndex - 1)
+            System.arraycopy(args, minecraftVersionIndex + 1, args2, minecraftVersionIndex - 1, args.size - minecraftVersionIndex - 1)
+            mainMethod.invoke(null, args2)
         }
 
     }
